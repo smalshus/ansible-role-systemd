32284e6213749a2031c5a1866a11a438ad064285
Ahmad Wilson <zer0ne.io.x@gmail.com>
2025-01-07 00:02:07 -0600

    chore: remove deprecated kitchen ruby tests

--- tests/molecule/uninstall/tests/test_service_uninstall.py
+++ tests/molecule/uninstall/tests/test_service_uninstall.py
@@ -1,7 +1,7 @@
 def test_service_removed(host):
+    service = host.service("test-service")
+
     try:
-        service = host.service("test-service")
-        assert not service.is_running, "test-service should not be running after uninstallation"
         assert not service.is_enabled, "test-service should not be enabled after uninstallation"
     except AssertionError as e:
         # Handle unexpected exit code due to service not found
